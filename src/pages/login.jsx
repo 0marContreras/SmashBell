c7247997a9979a5ed1eb921b7a3ab399665234fe
vocontreras <cd.victor.omar@gmail.com>
2023-02-04 19:05:21 -0600

    fixeado bug

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -145,7 +145,7 @@ export default function Login(){
             </button>
 
             <button onClick={handleTwitchSignIn} type="button" className="group relative flex w-full justify-center focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-            <svg className="mr-3 h-5 w-5 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="3"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 2H3v16h5v4l4-4h5l4-4V2zM11 11V7M16 11V7" /></svg>
+            <svg className="mr-3 h-5 w-5 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="3"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M21 2H3v16h5v4l4-4h5l4-4V2zM11 11V7M16 11V7" /></svg>
             Sign in with Twitch</button>
           </form>
         </div>
