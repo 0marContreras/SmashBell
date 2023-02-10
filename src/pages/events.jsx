0d756e285c5c74b37c43629362ece079f4e34f22
Paulina Bastista <uwupaulina09@gmail.com>
2023-02-10 12:25:44 -0700

    maquetacion

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,8 +1,21 @@
-export default function Event(){
+import Image from 'next/image'
+export default function Event() {
     return(
         <>
-        <h1>Profile page</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita explicabo, eligendi perferendis soluta obcaecati asperiores, voluptatibus nam optio rerum deleniti quas laborum voluptas, cumque reiciendis tenetur placeat quae eveniet consequatur.</p>
+        <h1>Events</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita explicabo, eligendi perferendis soluta obcaecati asperiores, voluptatibus nam optio rerum deleniti quas laborum voluptas, cumque reiciendis tenetur placeat quae eveniet consequatur.</p>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><Image src={require('../../public/Images/smash.jpg')} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">UtchBiSmash</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa veniam numquam nostrum iste odio earum provident maiores cupiditate quae. Quae atque sed dolore pariatur quam earum nihil veniam perspiciatis natus!</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">View More</button>
+                    </div>
+                </div>
+            </div>
         </>
+        
     )
+    
 }
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,8 +1,47 @@
-export default function Profile(){
+import Image from 'next/image'
+
+export default function Profile() {
     return(
         <>
+            <div className="prose">
+
         <h1>Profile page</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita explicabo, eligendi perferendis soluta obcaecati asperiores, voluptatibus nam optio rerum deleniti quas laborum voluptas, cumque reiciendis tenetur placeat quae eveniet consequatur.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita explicabo, eligendi perferendis soluta obcaecati asperiores, voluptatibus nam optio rerum deleniti quas laborum voluptas, cumque reiciendis tenetur placeat quae eveniet consequatur.</p>
+            
+            </div>
+            
+
+
+            <div className="card w-96 bg-gray-800 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <div className="avatar">
+                        <div className="w-24 rounded-full">
+                            <Image src={require('../../public/Images/marth.jpg')} />
+                        </div>
+                    </div>  
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Omar Contreras</h2>
+                    <p>El mas honesto, el heroe de todos los niños</p>
+
+                
+
+
+                    <div tabIndex={0} className="collapse border border-base-300 bg-base-100 rounded-box">
+                        <div className="collapse-title text-xl font-medium">
+                            Edit profile
+                        </div>
+                        <div className="collapse-content">
+                            <input type="text" placeholder="New name" className="input input-bordered input-info w-full max-w-xs " />
+                            <input type="text" placeholder="New description" className="mt-3 input input-bordered input-info w-full max-w-xs " />
+                            <input type="file" className="mt-3 file-input file-input-bordered file-input-primary w-full max-w-xs" />
+                            <button className="mt-4 relative flex w-full btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
+            
         </>
     )
 }
