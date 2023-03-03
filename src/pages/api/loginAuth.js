bd5e4dd4f0a4b80b756474c658e46a59fb1e0edc
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-03-02 21:07:15 -0600

    Created cookie system

--- src/pages/api/loginAuth.js
+++ src/pages/api/loginAuth.js
@@ -1,3 +1,6 @@
+import jwt from 'jsonwebtoken'
+import { serialize } from 'cookie'
+
 export default function handler(req, res) {
     // Get data submitted in request's body.
     const body = req.body
@@ -31,7 +34,28 @@ export default function handler(req, res) {
       } else {
         // Check if the user exists and the password matches.
         if (results.length > 0 && bcrypt.compareSync(body.password, results[0].password)) {
+
+          //Creates a constant that will storage our token
+          const token = jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, //Set an expire date to our token
+            email: `${body.email}`, //we pass the session infromation  to the token
+            password: hashedPassword,
+          }, process.env.JWT_SECRET) //Pass the secret password/key to acces the token
+
+          const serialCookie = serialize('authCookie', token, {
+
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', //Needs a ssl certificate to work, but NODE_ENV checks if we are in production or not.
+            sameSite: 'strict',
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            path: '/'
+          });       //Serialize the token to transform it to a cookie
+
           console.log("Accedio");
+
+          //Make the cookie: we pass the Set-Cookie instruction
+          res.setHeader('Set-Cookie', serialCookie);
+
           return res.status(200).json({ message: 'accesado :D' });
           
         } else {
