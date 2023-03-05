c7a21234759bb22a17afa152ed9908458e258da1
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-03-05 16:07:47 -0600

    START GG API CONNECTION

---
+++ startgg.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const startggUrl = "https://api.start.gg/gql/alpha";
+const apiKey = process.env.STARTGG_KEY;
+
+const getEventId = (tournamentName, eventName) => {
+    const eventSlug = `tournament/${tournamentName}/event/${eventName}`;
+    let eventId;
+    fetch(startggUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+            Authorization: 'Bearer ' + apiKey
+        },
+
+        body: JSON.stringify({
+            query: "query EventQuery($slug:String) {event(slug: $slug) {id name}}",
+            variables: {
+                slug: eventSlug
+            },
+        })
+    }).then(r => r.json())
+    .then(data => {
+        console.log(data.data);
+        eventId = data.data.event.id;
+    })
+
+    return eventId;
+}
+
+const getCompletedMatches = (eventId) => {
+
+    fetch(startggUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+            Authorization: 'Bearer ' + apiKey
+        },
+
+        body: JSON.stringify({
+            query: "query EventSets($eventId: ID!, $page: Int!, $perPage: Int!) { event(id: $eventId) {sets(page: $page perPage: $perPage sortType: STANDARD) {pageInfo {total} nodes {id slots {entrant {name}}}}}}",
+            variables: {
+                eventId: 810860,
+                page: 1,
+                perPage: 5
+            },
+        })
+    }).then(r => r.json())
+    .then(data => {
+        console.log(data.data);
+        console.log(data.data.event.sets.nodes[0])
+    })
+}
+
+
+
+
+
+//https://www.start.gg/tournament/revenge-round-8/event/ultimate-singles
+getEventId('revenge-round-8', 'ultimate-singles');
+getCompletedMatches(810860);
