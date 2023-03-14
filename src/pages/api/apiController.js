849c5cc4ae2b6eb241afbae70cb3a6229b506639
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-03-13 22:19:12 -0600

    Api painted in events

--- src/pages/api/apiController.js
+++ src/pages/api/apiController.js
@@ -2,34 +2,31 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const startggUrl = "https://api.start.gg/gql/alpha";
 const apiKey = '1ab56b87f62bb1948ed4bb0c786618fa';
+var respuesta = new Array();
 
-export function getEvents(eventNum){
-    let respuesta = new Array();
-    fetch(startggUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'Accept': 'application/json',
-            Authorization: 'Bearer ' + apiKey
-        },
-
-        body: JSON.stringify({
-            query: 'query TournamentsByVideogames($perPage: Int, $videogameIds: [ID]) {tournaments(query: {perPage: $perPage page: 1 sortBy: "startAt asc" filter: {upcoming: true videogameIds: $videogameIds}}) {nodes {id name slug}}}',
-            variables: {
-                videogameIds: [1],
-                page: 1,
-                perPage: 100
-            },
-        })
-    }).then(r => r.json())
-    .then(data => {
-        for(let i = 0; i <= eventNum; i++){
-        //console.log(data.data.tournaments.nodes[i]);
-        respuesta[i] = data.data.tournaments.nodes[i];
-        
-
-    }
-    //console.log(respuesta);
+export function getEvents(eventNum) {
+  return fetch(startggUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'Accept': 'application/json',
+      Authorization: 'Bearer ' + apiKey
+    },
+    body: JSON.stringify({
+      query: 'query TournamentsByVideogames($perPage: Int, $videogameIds: [ID]) {tournaments(query: {perPage: $perPage page: 1 sortBy: "startAt asc" filter: {upcoming: false videogameIds: $videogameIds}}) {nodes {id name slug}}}',
+      variables: {
+        videogameIds: [1386],
+        page: 1,
+        perPage: 100
+      },
     })
-    return respuesta
+  })
+    .then(r => r.json())
+    .then(data => {
+      const respuesta = [];
+      for (let i = 0; i <= eventNum; i++) {
+        respuesta[i] = data.data.tournaments.nodes[i];
+      }
+      return respuesta;
+    });
 }
--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,135 +1,57 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import { useSession} from "next-auth/react"
+import { useSession } from "next-auth/react"
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getEvents } from './api/apiController'
 import NavBar from './navbarTest'
 import Footer from './footer'
-export default function Event(){
 
-    let events = new Array();
+export default function Event() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    getEvents(15)
+      .then((events) => {
+        //console.log(events);
+        setEvents(events);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const cardItems = events.map((evento, i) => (
     
-    useEffect(() => {
-         events =  getEvents(3);
-      }, []);
+            <div className='flex mx-4'>
+            <br />
+                <div className="card w-96 glass text-center text-neutral-content">   
+                    <figure><Image src={require('../../public/./Images/saitama.jpg')} alt="car!"/></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{evento.name}</h2>
+                            <p>How to park your car at your garage?</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">View Event</button>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+                
+  ));
 
-    return(
-        <>
-        
-       
-         <NavBar/>
-        
-        <div className='p-10'>
-                      {/*AQUI EMPIEZA EL LISTADO DE CARDS*/}   
-                        <div className='flex'>
-                            <div className='flex mx-4'>
-                            <br />
-                                <div className="card w-96 glass text-center text-neutral-content">   
-                                    <figure><Image src={require('../../public/./Images/cat.jpg')} alt="car!"/></figure>
-                                        <div className="card-body">
-                                            <h2 className="card-title">sssss</h2>
-                                            <p>How to park your car at your garage?</p>
-                                            <div className="card-actions justify-end">
-                                                <button className="btn btn-primary">Learn now!</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <br />
-                                </div>
-                                    <div className='flex mx-12'>
-                                    <br />
-                                        <div className='container block-line flex'>
-                                            <div className="card w-96 glass text-center text-neutral-content">   
-                                                <figure><Image src={require('../../public/./Images/Orquesta.jpg')} alt="car!"/></figure>
-                                                    <div className="card-body">
-                                                        <h2 className="card-title">YAYAYAYAYYA</h2>
-                                                        <p>How to park your car at your garage?</p>
-                                                        <div className="card-actions justify-end">
-                                                            <button className="btn btn-primary">Learn now!</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+  return (
+    <>
 
-                                        <div className='flex mx-12'>
-                                        <br />
-                                            <div className='container block-line flex'>
-                                                <div className="card w-96 glass text-center text-neutral-content">   
-                                                    <figure><Image src={require('../../public/./Images/Gatodiablo.png')} alt="car!"/></figure>
-                                                        <div className="card-body">
-                                                            <h2 className="card-title">Life hack</h2>
-                                                            <p>How to park your car at your garage?</p>
-                                                            <div className="card-actions justify-end">
-                                                                <button className="btn btn-primary">Learn now!</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <br />
-                                        </div>
-                                        <br/>
-                                        <div ></div>
-                                        <div className='flex'>
-                                            <div className='flex mx-4'>
-                                            <br />
-                                            <div className='container block-line flex'>
-                                                <div className="card w-96 glass text-center text-neutral-content">   
-                                                    <figure><Image src={require('../../public/./Images/Saitama.jpg')} alt="car!"/></figure>
-                                                        <div className="card-body">
-                                                            <h2 className="card-title">Life hack</h2>
-                                                            <p>How to park your car at your garage?</p>
-                                                            <div className="card-actions justify-end">
-                                                                <button className="btn btn-primary">Learn now!</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        <br />
-                                        <div className='flex mx-12'>
-                                            <br />
-                                            <div className='container block-line flex'>
-                                                <div className="card w-96 glass text-center text-neutral-content">   
-                                                    <figure><Image src={require('../../public/./Images/ok.jpg')} alt="car!"/></figure>
-                                                        <div className="card-body">
-                                                            <h2 className="card-title">Life hack</h2>
-                                                            <p>How to park your car at your garage?</p>
-                                                            <div className="card-actions justify-end">
-                                                                <button className="btn btn-primary">Learn now!</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        <br />
-                                        <div className='flex mx-12'>
-                                            <br />
-                                            <div className='container block-line flex'>
-                                                <div className="card w-96 glass text-center text-neutral-content">   
-                                                    <figure><Image src={require('../../public/./Images/Genos.jpg')} alt="car!"/></figure>
-                                                        <div className="card-body">
-                                                            <h2 className="card-title">Life hack</h2>
-                                                            <p>How to park your car at your garage?</p>
-                                                            <div className="card-actions justify-end">
-                                                                <button className="btn btn-primary">Learn now!</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        <br />
-                                        <br />                                
-                                    </div>
-                                </div>
-                        
-                        {/*AQUI EMPIEZA EL PIE DE PAGINA*/}
-            <Footer/>
- 
-            {/*AQUI TERMINA EL PIE DE PAGINA*/}
-     </>
-    )
+      <NavBar />
+      
+      
+      <div className='p-10'>
+      <div className="grid grid-cols-4 gap-4">
+        <>{cardItems}</>
+        </div>
+        </div>
+
+      <Footer />
+    </>
+  );
 }
