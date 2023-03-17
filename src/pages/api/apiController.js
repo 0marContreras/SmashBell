64fc6165117e9e99e17f0edcc05442df87018355
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2023-03-16 22:44:58 -0600

    Api connections

--- src/pages/api/apiController.js
+++ src/pages/api/apiController.js
@@ -30,3 +30,5 @@ export function getEvents(eventNum) {
       return respuesta;
     });
 }
+
+
---
+++ src/pages/api/twitterController.js
@@ -0,0 +1,25 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'JeCS4DTF2cHcLzJJzWLLIOb2Y',
+    consumer_secret: 'pLgTnBOX07nB87hRZNC7gNS6eHfplb6Da9Xzj5gFx6yfVn3w6T',
+    access_token_key: '1437909112709332992-yr5z1edtQYS2FLxZHOqvw5vHovlI4S',
+    access_token_secret: 'OHiSXtdOT6ZCJI7SYDPqRVChj8xcAd36Zaw3KIVyvhID6'
+  });
+
+  export default function handler(req, res) {
+   
+    client.get('search/tweets', {q: 'MKLeo'}, function(error, tweets, response) {
+      if (error) {
+        console.log(error);
+        res.status(500).send(error);
+      } else {
+        // Obtener la URL de cada tweet
+        console.log(response)
+        const urls = tweets.statuses.map((tweet) => {
+          return `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+        });
+        res.status(200).json(urls);
+      }
+    });
+  }
